f8865377565fa509cf3ff017ba57c89d9560d911
dko <im.shaedar@gmail.com>
2022-08-23 23:46:32 +0200

    Saving stuff to chrome.storage.local as well

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Changes links to office files to URI format which allow these files to be opened
   After plugin is installed, you'll need to add your legacy sharepoint site urls to the list of sites by clicking on a plugin icon.
 
 #### Version history: ####
+* 0.9: Preparing to migrate to manifest v3
 * 0.8: Removed unneeded permission and got rid of underscore.js
 * 0.7: Bug with loss of configuration upon browser restart is fixed
 * 0.6: Visio files support added
--- src/RulesService.js
+++ src/RulesService.js
@@ -2,6 +2,11 @@ var LocalRulesService = function (){
 		this.get = function(){
 			var storedRules = localStorage['rules'];
 
+			console.warn("Local storage rules = " + storedRules);
+
+			//Preparing to migrate to chrome.storage.local
+			chrome.storage.local.set({'rules': storedRules}, function() {});
+
 			if(!storedRules){
 				return [];
 			}
@@ -9,7 +14,11 @@ var LocalRulesService = function (){
 			return JSON.parse(storedRules);
 		};
 		this.set = function(rules){
-			localStorage['rules'] = JSON.stringify(rules);
+			var rulesToStore = JSON.stringify(rules);
+			localStorage['rules'] = rulesToStore;
+
+			//Preparing to migrate to chrome.storage.local
+			chrome.storage.local.set({'rules': rulesToStore}, function() {});
 		};
 };
 
